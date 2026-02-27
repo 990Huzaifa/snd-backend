90735f06756f6cca85abe5891e7909451a64eab4
syed-huzaifa-ali <syedhuzaifaali366@gmail.com>
2026-02-27 16:04:28 +0500

    changes updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { MasterDbModule } from './master-db/master-db.module';
 import { PlatformModule } from './platform/platform.module';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -13,5 +15,7 @@ import { CommonModule } from './common/common.module';
     AuthModule,
     PlatformModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
